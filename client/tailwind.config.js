0ec1287361cf8267cf0447142994d7696cd45d39
Sayantan Kabri <sayantankabri108@gmail.com>
2024-12-16 09:50:52 +0530

    import promlen tailwind config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindScrollbar from 'tailwind-scrollbar';
+
 export default {
-  darkMode:"class",
+  darkMode: "class",
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -14,38 +16,36 @@ export default {
           text: '#E0E0E0', // Light text
           muted: '#B3B3B3', // Muted text
           border: '#292929', // Border color
-          codeBg:'#111827',
-          btnc:'#121d36',
-          cardboder:'#103A5C'
+          codeBg: '#111827',
+          btnc: '#121d36',
+          cardboder: '#103A5C'
         },
-    },
-    backgroundImage: {
-      'dark-gradient': 'linear-gradient(to right, #0f172a, #152648)',
-      'light-gradient': 'linear-gradient(to right, #ffffff, #e0f7fa)',
-    },
-    animation: {
-      'rocket-launch': 'rocket 2s linear infinite',
-      'beat': 'beat 1s ease-in-out infinite',
-    },
-    keyframes: {
-      rocket: {
-        '0%': { transform: 'translateY(0)' },
-        '50%': { transform: 'translateY(-20px)' },
-        '100%': { transform: 'translateY(0)' },
       },
-      beat: {
-        '0%, 100%': { transform: 'scale(1)' },
-        '50%': { transform: 'scale(1.2)' },
+      backgroundImage: {
+        'dark-gradient': 'linear-gradient(to right, #0f172a, #152648)',
+        'light-gradient': 'linear-gradient(to right, #ffffff, #e0f7fa)',
       },
-    }
-
+      animation: {
+        'rocket-launch': 'rocket 2s linear infinite',
+        'beat': 'beat 1s ease-in-out infinite',
+      },
+      keyframes: {
+        rocket: {
+          '0%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-20px)' },
+          '100%': { transform: 'translateY(0)' },
+        },
+        beat: {
+          '0%, 100%': { transform: 'scale(1)' },
+          '50%': { transform: 'scale(1.2)' },
+        },
+      }
     },
-    
   },
   plugins: [
     tailwindScrollbar,
   ],
-  variants:{
-    scrollbar:['dark']
+  variants: {
+    scrollbar: ['dark']
   }
-}
+};
